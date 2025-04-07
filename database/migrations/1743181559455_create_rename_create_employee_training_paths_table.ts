bb7674a3aa5fe9876cd4f3ea6698610c9b4e1797
Héctor Emilio Velásquez Pérez <hectoremilio1000@gmail.com>
2025-04-07 16:12:14 -0600

    cambios

--- database/migrations/1743181559455_create_rename_create_employee_training_paths_table.ts
+++ database/migrations/1743181559455_create_rename_create_employee_training_paths_table.ts
@@ -2,11 +2,11 @@ import { BaseSchema } from '@adonisjs/lucid/schema'
 
 export default class RenameemployeeTabletrainingEmployee extends BaseSchema {
   public async up() {
-    this.schema.renameTable('create_employee_training_paths', 'training_employee_paths')
+    this.schema.renameTable('employee_training_paths', 'training_employee_paths')
   }
 
   public async down() {
     // revertir el cambio
-    this.schema.renameTable('training_employee_paths', 'create_employee_training_paths')
+    this.schema.renameTable('training_employee_paths', 'employee_training_paths')
   }
 }
