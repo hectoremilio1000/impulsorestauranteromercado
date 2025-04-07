682b29ff96b9dd76a015059cc28fdb971f4f282d
Héctor Emilio Velásquez Pérez <hectoremilio1000@gmail.com>
2025-04-07 15:18:46 -0600

    cambios env

--- start/env.ts
+++ start/env.ts
@@ -18,6 +18,8 @@ export default await Env.create(new URL('../', import.meta.url), {
   HOST: Env.schema.string({ format: 'host' }),
   LOG_LEVEL: Env.schema.enum(['fatal', 'error', 'warn', 'info', 'debug', 'trace']),
 
+  FRONTEND_URL: Env.schema.string(),
+
   /*
   |----------------------------------------------------------
   | Variables for configuring database connection
