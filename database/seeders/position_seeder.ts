f945690d7111184c4c5d9296683535c95479d02a
Héctor Emilio Velásquez Pérez <hectoremilio1000@gmail.com>
2025-04-22 22:52:27 -0600

    cambios seeder

--- database/seeders/position_seeder.ts
+++ database/seeders/position_seeder.ts
@@ -1,10 +1,10 @@
-// database/seeders/PositionSeeder.ts
-import Position from '#models/position'
+// database/seeders/position_seeder.ts
 import { BaseSeeder } from '@adonisjs/lucid/seeders'
+import Position from '#models/position'
 
-export default class PositionSeeder extends BaseSeeder {
+export default class extends BaseSeeder {
   public async run() {
-    await Position.createMany([
+    await Position.updateOrCreateMany('nombre', [
       { nombre: 'Mesero', tipo: 'piso', version: 1, activo: true },
       { nombre: 'Gerente', tipo: 'piso', version: 1, activo: true },
       { nombre: 'Subgerente', tipo: 'piso', version: 1, activo: true },
@@ -18,6 +18,33 @@ export default class PositionSeeder extends BaseSeeder {
       { nombre: 'Jefe de Cocina', tipo: 'cocina', version: 1, activo: true },
       { nombre: 'Capitán', tipo: 'piso', version: 1, activo: true },
       { nombre: 'Garrotero', tipo: 'piso', version: 1, activo: true },
+
+      // — Nuevos registros —
+      { nombre: 'Auxiliar administrativo', tipo: 'administración', version: 1, activo: true },
+      { nombre: 'Community Manager', tipo: 'marketing', version: 1, activo: true },
+      {
+        nombre: 'Analista de marketing digital / Growth Hacker',
+        tipo: 'marketing',
+        version: 1,
+        activo: true,
+      },
+      {
+        nombre: 'Ejecutivo(a) de eventos y banquetes',
+        tipo: 'marketing',
+        version: 1,
+        activo: true,
+      },
+      { nombre: 'Gerente de marketing y ventas', tipo: 'marketing', version: 1, activo: true },
+      { nombre: 'Reclutador(a) / Talent Acquisition', tipo: 'rrhh', version: 1, activo: true },
+      { nombre: 'Analista de clima laboral / Engagement', tipo: 'rrhh', version: 1, activo: true },
+      { nombre: 'Jefe(a) de Recursos Humanos', tipo: 'rrhh', version: 1, activo: true },
+      { nombre: 'Head of People / Director(a) de Talento', tipo: 'rrhh', version: 1, activo: true },
+      {
+        nombre: 'Gerente administrativo-financiero',
+        tipo: 'administración',
+        version: 1,
+        activo: true,
+      },
     ])
   }
 }
