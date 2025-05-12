62924a2ba95e104eea8eb95ed2a55f144c61c135
Héctor Emilio Velásquez Pérez <hectoremilio1000@gmail.com>
2025-05-12 14:16:11 -0600

    cambios

--- app/validators/auth.ts
+++ app/validators/auth.ts
@@ -5,14 +5,22 @@ const password = vine.string().minLength(8)
 
 export const registerValidator = vine.compile(
   vine.object({
-    email: vine.string().unique(async (db, value, field) => {
-      const user = await db
-        .from('users')
-        .whereNot('id', field.meta.userId)
-        .where('email', value)
-        .first()
-      return !user
-    }),
+    email: vine
+      .string()
+      .email()
+      .normalizeEmail()
+      .unique(async (db, value, field) => {
+        const query = db.from('users').where('email', value)
+
+        // Solo excluye el ID cuando viene en meta (ej. al editar)
+        if (field.meta?.userId) {
+          query.whereNot('id', field.meta.userId)
+        }
+
+        return !(await query.first())
+      }),
+
+    password: vine.string().minLength(8),
   })
 )
 
